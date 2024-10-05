3d23066943abc7dad13864fe98fd155cff9720a3
akash <akashtayade5668@gmail.com>
2024-10-05 21:29:04 +0530

    new added

---
+++ src/Componants/Home.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='h-full w-full bg-zinc-300'>
+        <div className='flex flex-row h-[12] '>
+                <img className='size-[6rem]' src='https://th.bing.com/th/id/OIP.cVct29AGTimIwuuIiTI31AAAAA?rs=1&pid=ImgDetMain' alt='Diffence Logo' />
+                <img className='size-[6.3rem]' src='https://drdoaeroindia2023.com/img/dysl-ai.png' alt='Diffence Logo' />
+            <div className='flex-col ml-5'>
+                <p className='text-xl font-semibold text-[#2b4e9e]'>RAC</p>
+                <p className='text-sm font-normal'> Recruitment and Assessment Centre, DRDO</p>
+                <p className='text-sm font-normal'>Department of Defence Research and Development</p>
+                <p className='text-sm font-normal'> Ministry of Defence, Government of India</p>
+                <p className='text-sm font-normal'> an ISO 9001 certified establishment</p>
+            </div>
+        </div>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Home
